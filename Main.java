3d333cc336c633dd857621973f6917e58f81fa75
Tesa FV <53526469+deastrobooking@users.noreply.github.com>
2022-03-29 22:32:30 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+ 
+class Box {
+  double width, height, depth;
+  Box(double w, double h, double d){
+    width = w;
+    height = h;
+    depth = d;
+  }
+  double volume(){
+    return width * height * depth;
+  }
+}
+class OverloadCons {
+  public static void main(String[] args) {
+    Box mybox1 = new Box(10,20,15);
+    double vol; 
+    vol =mybox1.volume();
+    System.out.println("Vol of Box 1 is" + vol);
+    }
+}
