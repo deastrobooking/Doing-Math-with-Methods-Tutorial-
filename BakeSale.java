d118963731a04756f03a83f851ae21174ba33160
FRC ROBOTICS PAGE <53526469+deastrobooking@users.noreply.github.com>
2022-03-31 19:51:25 -0400

    a

---
+++ BakeSale.java
@@ -0,0 +1,20 @@
+public class BakeSale {
+	public static void main(String[] args) {   
+		int numCookies = 17;
+    numCookies -= 3;
+    // minus 3 and sets the var = to 14
+    numCookies /= 2;
+    // divides by 2 and sets equal to 7
+
+    // Add your code above
+    System.out.println(numCookies);
+	}
+}
+
+/*
+Addition (+=)
+Subtraction (-=)
+Multiplication (*=)
+Division (/=)
+Modulo (%=)
+*/
